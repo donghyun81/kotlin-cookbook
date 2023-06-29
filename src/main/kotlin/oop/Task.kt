84aca0d08d7bdf0caded15a325cc3f101a162a87
윤동현 <donghyunyoon81@gmail.com>
2023-06-29 16:58:31 +0900

    record<Task> : 3.1 const와 val의 차이 이해하기
     문제: 런타임 보다는 컴파일 타임에 변수가 상수임을 나타내야함(val는 변경 불가능하지만 이러한 할당이 실행 시간에 일어남 )
     해법: 컴파일 타임 상수 const 변경자를 사용
     설명:
            val sum = a+b 처럼 a와 b 값에 따라 런타임시에 값이 정해지지만
            const val 객체 또는  동반 객체 선언의 최상위 속성 또는 멤버여야 하므로 a,b보다 상위에 선언되어 있어서
            const val sun = a+b로 할당이 불가능하다
            이를 통해서 const는 private,inline처럼 변경자(접근제한자)임을 알 수 있다.
            (먼저 선언된 const를 통해서 const에 할당이 가능하나 그럼 const를 사용하는 의미가 사라진다)

--- src/main/kotlin/oop/Task.kt
+++ src/main/kotlin/oop/Task.kt
@@ -7,18 +7,19 @@ fun main(){
 class Task(val name: String, _priority: Int = DEFAULT_PRIORITY) {
 
     companion object {
+        // const(컴파일 타임 상수)는 객체나 동반 객체(companion object) 선언의 최상위 속성 또는 멤버여야 한다
         const val MIN_PRIORITY = 1
         const val MAX_PRIORITY = 5
         const val DEFAULT_PRIORITY = 3
     }
 
-    var priority = validPriority(_priority)
+    var priority = validPriority(_priority) // 사용자 정의 설정(setter)
         set(value) {
             field = validPriority(value)
         }
 
-    private fun validPriority(p: Int) =
-        p.coerceIn(MIN_PRIORITY, MAX_PRIORITY) // 최소보다 작을경우 민 최대보다 클 경우 max 아닐경우 그냥 return
+    private fun validPriority(p: Int) = // private 선언된 우선순위 검증 함수
+        p.coerceIn(MIN_PRIORITY, MAX_PRIORITY) // 최소보다 작을경우 MIN_PRIORITY 반환 최대보다 클 경우 MAX_PRIORITY 반환
 
 
 //    private fun validPriority(p: Int) = when {

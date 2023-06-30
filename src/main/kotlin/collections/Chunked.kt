5cf650b8e20cc86c26036cf8814ca3e3bed9ad0b
윤동현 <donghyunyoon81@gmail.com>
2023-06-30 16:49:34 +0900

    record<Chunked> : 컬렉션을 윈도우로 처리하기 문제 : 값 컬렉션이 주어진 경우 컬렉션을 횡단하는 작은 윈도우를 이용해 컬렉션을 처리해야 한다. 해법 : 같은 크기로 나누기만 한다면 chunked , 정해진 간격으로 컬렉션을 따라 움직이면서 나눈다면 windowed 설명 :  chunked(size: Int) -> 컬렉션을 사이즈만큼 나눠서 반환 (함수 내부의 반환 형태 return windowed(size, size, partialWindows = true)) 	windowed(size: Int, step: Int = 1, partialWindows: Boolean = false) : 	size: 사이즈만큼 나눈다 	step: 컬렉션이 얼마나 이동해서 나눌것인지 정한다 	partialWindows: 마지막 부분이 size의 크기보다 낮을 경우 유지해서 반환할것인지 버릴것인지 정한다.

--- src/main/kotlin/collections/Chunked.kt
+++ src/main/kotlin/collections/Chunked.kt
@@ -6,12 +6,14 @@ fun main() {
     val pieces = list.chunked(3)
     println(pieces)
 
-    val sums = list.chunked(3) { it.sum() }
+    val sums = list.chunked(3) { it.sum() } //인자값 만큼 나누기(마지막이 인자값만큼 없으면 없는데로 반환)
     println("Sums: $sums")
 
-    println(list.windowed(3, 1))
+    println(list.windowed(3, 1)) // size 만큼 나누지만 step 만큼 이동하면서 나누기 1,2,3,4 (3,1)일 경우 [1,2,3] [2,3,4]
+    //(마지막이 인자값만큼 없으면 버린다)
     println("Averages: " + list.windowed(3, 1) { it.average() })
 
-    println(list.windowed(3, 3))
+    println(list.windowed(3, 3)) // size 만큼 나누지만 step 만큼 이동하면서 나누기 1,2,3,4 (3,3)일 경우 [1,2,3]
+    ////(마지막이 인자값만큼 없으면 버린다)
     println("Averages: " + list.windowed(3, 3) { it.average() })
 }

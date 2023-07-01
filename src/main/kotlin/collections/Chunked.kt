7b12e807bdb0e6a2d1f912037a452da9b4780bbd
윤동현 <donghyunyoon81@gmail.com>
2023-07-01 11:57:05 +0900

    record : 5.8 리스트 구조 분해 문제 : 리스트 원소에 접근할 수 있게 구조 분해(destructuring)해야 한다 해법 : 최대 5개의 원소를 가진 그룹에 리스트 할당한다 설명 : val list = listOf(1,2,3,4,5,6,7) 	val (a,b,c,d,e) = list 	a = 1 , b = 2 ,c = 3 ... 으로 변수에 리스트의 값이 할당된다

--- src/main/kotlin/collections/Chunked.kt
+++ src/main/kotlin/collections/Chunked.kt
@@ -6,14 +6,14 @@ fun main() {
     val pieces = list.chunked(3)
     println(pieces)
 
-    val sums = list.chunked(3) { it.sum() } //인자값 만큼 나누기(마지막이 인자값만큼 없으면 없는데로 반환)
+    val sums = list.chunked(3) { it.sum() } //인자값 만큼 나누기(마지막이 인자값만큼 없으면 해당 부분만 반환)
     println("Sums: $sums")
 
-    println(list.windowed(3, 1)) // size 만큼 나누지만 step 만큼 이동하면서 나누기 1,2,3,4 (3,1)일 경우 [1,2,3] [2,3,4]
+    println(list.windowed(3, 1,false)) // size 만큼 나누지만 step 만큼 이동하면서 나누기 1,2,3,4 (3,1)일 경우 [1,2,3] [2,3,4]
     //(마지막이 인자값만큼 없으면 버린다)
     println("Averages: " + list.windowed(3, 1) { it.average() })
 
-    println(list.windowed(3, 3)) // size 만큼 나누지만 step 만큼 이동하면서 나누기 1,2,3,4 (3,3)일 경우 [1,2,3]
-    ////(마지막이 인자값만큼 없으면 버린다)
+    println(list.windowed(3, 3,true)) // size 만큼 나누지만 step 만큼 이동하면서 나누기 1,2,3,4 (3,3)일 경우 [1,2,3]
+    ////(마지막이 인자값만큼 없으면 해당 부분만 반환)
     println("Averages: " + list.windowed(3, 3) { it.average() })
 }

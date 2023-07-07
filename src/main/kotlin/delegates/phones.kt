393e95786b343b80d7028db5ed3841fce4ad2f89
윤동현 <donghyunyoon81@gmail.com>
2023-07-07 22:11:40 +0900

    record<phones> : 8.2 lazy 대리자 사용하기
    문제 : 어떤 속성이 필요할 때까지 해당 속성의 초기화를 지연시키고 싶다.
    해법 : 코틀린 표준 라이브러리의 lazy 대리자를 사용하자.
    설명 : 어떤 속성의 획득자와 설정자가 대리자라고 불리는 다른 객체에서 구현되어 있다는것을 암시하기 위해 by 키워드를 사용 (interface by impl)
    	다수의 대리자 함수중 lazy를 사용하여 초기화 지연을 실행한다.
    	lazy 대리자 함수 정의
    	1. fun <T> lazy(initializer: () -> T) : Lazy<T> 기본으로 스스로 동기화 한다(mode = LazyThreadSafetyMode.SYNCHRONIZED 이다)
    	2. fun <T> lazy(mode: LazyThreadSafetyMode, initalizer: () ->) : Lazy<T> (mode를 설정할 수 있다)
    	SYNCHRONIZED : 하나의 스레드만 Lazy 인스턴스를 초기화할 수 있게 락을 사용
    	PUBLICATION : 초기화 함수가 여러 번 호출될 수 있지만 첫 번째 리턴값만 사용
    	NONE : 락이 사용되지 않음
    	3. fun <T> lazy(lock:Any?,  initalizer: () ->) : Lazy<T>
    	lock으로 지정한 객체로 synchronized(lock) { 반환} 실행

--- src/main/kotlin/delegates/phones.kt
+++ src/main/kotlin/delegates/phones.kt
@@ -2,6 +2,7 @@ package delegates
 
 interface Dialable {
     fun dial(number: String): String
+
 }
 
 class Phone : Dialable {
@@ -26,6 +27,10 @@ class SmartPhone(
 ) : Dialable by phone, Snappable by camera // by 키워드를 통해서 인자값의 객체의 함수들에 접근하여 자동으로 구현해준다.
 
 fun main() {
+    val a:Int by lazy (LazyThreadSafetyMode.PUBLICATION){ println("a값 할당되었습니다") ;1} // 해당 변수에 접근하여 값을 사용하기전까지 1을 반환하는 계산을 하지않음 tip) 함수 실행은 한번만 된다
+    val b:Snappable by lazy (SmartPhone()){ println("a값 할당되었습니다") ;Camera()} // 해당 변수에 접근하여 값을 사용하기전까지 1을 반환하는 계산을 하지않음 tip) 함수 실행은 한번만 된다
+    println(b.takePicture())
+    println(a)
     val myPhone = SmartPhone()
     println(myPhone.a)
     //println(myPhone.b) Snappable 타입 객체인 camera는 Camera()에만 존재하는 b변수,다른 각종 함수에 접근할 수 없다.

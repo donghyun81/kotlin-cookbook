350ac0e90a120f178d8a22a2a4f58c16714c1f8f
윤동현 <donghyunyoon81@gmail.com>
2023-07-07 20:57:42 +0900

    record<phones> : 8.1 대리자를 사용해서 합성 구현하기
    문제 : 다른 클래스의 인스턴스가 포함된 클래스를 만들고, 그 클래스에 연산을 위임하고 싶다.
    해법 : 연산을 위임할 메소드가 포함된 인터페이스를 만들고, 클래스에서 해당 인터페이스를 구현한 다음, by 키워드를 사용해 바깥쪽에 래퍼 클래스를 만든다.
    설명 : 최신 객체 지향 디자인은 강한 결합으로 인해 상위 객체의 수정으로 인한 오류를 줄이기 위해서 상속보다는 위임을 선호한다.
    	코틀린에서 by 키워드는 포함된 객체에 있는 모든 public 함수를 이 객체를 담고 있는 컨테이너를 통해 노출할 수 있다.
    	interface를 만들고 interface의 구현 객체를 만든뒤에 구현 객체를 위임 받아 사용할 객체를 만든다.
    	위임 받을 객체의 인자의 기본값으로 구현객체를 할당 받는다.
    	사용 객채 : interface by (구현객체) 인자 ,.. 식으로 위임받아서 위임 받은 객체들의 함수나 변수에 접근 할 수 있다.
     	tip) interface는 다양한 구현객체를 받을수 있지만 interface타입으로 위임 받을 경우에 구현객체가 override한 속성과 함수만 접근 할 수 있다.

--- src/main/kotlin/delegates/phones.kt
+++ src/main/kotlin/delegates/phones.kt
@@ -9,20 +9,26 @@ class Phone : Dialable {
 }
 
 interface Snappable {
+    val a: Int
+
     fun takePicture(): String
 }
 
 class Camera : Snappable {
+    override val a = 0
+    val b = 0
     override fun takePicture() = "Taking picture..."
 }
 
 class SmartPhone(
-    private val phone: Dialable = Phone(),
-    private val camera: Snappable = Camera()
-) : Dialable by phone, Snappable by camera
+    private val phone: Dialable = Phone(),  // Dialable을 구현한 Phone()객체를 기본 인자값으로 할당
+    private val camera: Snappable = Camera() // Snappable을 구현한 Camera() 객체를 기본 인자값으로 할당
+) : Dialable by phone, Snappable by camera // by 키워드를 통해서 인자값의 객체의 함수들에 접근하여 자동으로 구현해준다.
 
 fun main() {
     val myPhone = SmartPhone()
+    println(myPhone.a)
+    //println(myPhone.b) Snappable 타입 객체인 camera는 Camera()에만 존재하는 b변수,다른 각종 함수에 접근할 수 없다.
     println(myPhone.dial("555-1234"))
     println(myPhone.takePicture())
 }
